f936a691ee74c5c1ad1ffde401b4fd923f3060b9
Marcin <marcin.Pucilowski@infotech.edu.pl>
2025-03-27 14:20:49 +0100

    Dodanie klas

--- Program.cs
+++ Program.cs
@@ -3,7 +3,20 @@ namespace Parking
     class Program 
     {
         static void Main() {
-            
+
         }
+    }
+
+    class ParkingLot {
+        private double ParkingSpots { get; set; }
+    }
+
+    class ParkingSpot {
+        private bool IsOccupied { get; set; }   
+    }
+
+    class Vehicle {
+        private string Marka {get; set;}
+        private double numer_rejestrcyjny {get; set;}
     }
 }
